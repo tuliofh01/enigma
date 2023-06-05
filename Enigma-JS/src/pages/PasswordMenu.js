4d4704ac9155768f1eae04e09cc6de35d51f0049
tuliofh01 <tuliofh01@gmail.com>
2023-06-05 14:40:09 -0300

    Refresh button

--- Enigma-JS/src/pages/PasswordMenu.js
+++ Enigma-JS/src/pages/PasswordMenu.js
@@ -74,6 +74,15 @@ function PasswordMenu(){
               />
               <span className="material-symbols-outlined">search</span>
             </button>
+            <button className={styles.searchButton} onClick={async () => {
+              setPasswords(await ipcRenderer.invoke("getPasswords"));
+            }}>
+              <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+              />
+              <span class="material-symbols-outlined">refresh</span>
+            </button>
           </div>
           <table>
             <tbody>
