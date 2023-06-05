cb0ae2343d5e10b788736d1f4ca415c0672e44b7
tuliofh01 <tuliofh01@gmail.com>
2023-06-05 09:58:45 -0300

    Readme

--- Enigma-JS/src/App.js
+++ Enigma-JS/src/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.js";
 import PasswordMenu from "./pages/PasswordMenu.js";
@@ -35,7 +12,6 @@ function App() {
           <Route path="/pwdMenu" element={<PasswordMenu/>} />
         </Routes>
     </Router>
->>>>>>> master
   );
 }
 
